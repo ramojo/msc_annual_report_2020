c172ecde6cd7c687f984382ec5f667e354e22da8
ramojo <amramogo@gmail.com>
2020-07-17 10:50:03 +0300

    Fixed access to Report Form and date for reporting

--- index.php
+++ index.php
@@ -196,7 +196,7 @@ div.copyright {
 <!--					<h1>MSc Annual Report</h1>-->
 			<p>All icipe MSc scholars who have not yet successfully defended their thesis must prepare an annual report. The purpose of the report is to assess progress through the programme in relation to clearly articulated expectations and milestones, and give scholars feedback on their progress.</p>
 				
-			<p>You can download an Annual Report Form here: <a href="http://cbid.icipe.org/apps/mscc_annual_report_2020/resources/MSc_icipe_Annual_Report_Form_2020.docx
+			<p>You can download an Annual Report Form here: <a href="http://cbid.icipe.org/apps/msc_annual_report_2020/resources/MSc_icipe_Annual_Report_Form_2020.docx
 "><b>Annual Report Form</b></a><p>
 
 			<p>The form consists of three parts. Part 1 is to be completed by the MSc Scholar; Part 2 by the Primary icipe Supervisor; and Part 3 by the Primary University Supervisor.</p>
@@ -215,9 +215,9 @@ div.copyright {
 </div>
 
 	<ol>
-				<li><p>Download the <a href="http://cbid.icipe.org/apps/MSc_annual_report_2020/resources/MSc_icipe_Annual_Report_Form_2020.docx"><b> Annual Report Form</b></a>.</p></li>	
+				<li><p>Download the <a href="http://cbid.icipe.org/apps/msc_annual_report_2020/resources/MSc_icipe_Annual_Report_Form_2020.docx"><b> Annual Report Form</b></a>.</p></li>	
 				<li><p>Complete PART 1 of the form, and sign and date.</p></li>
-				<li><p>After completing PART 1, send the <u>entire</u> form as a Word document to your <em>icipe</em> Primary Supervisor and your University Primary Supervisor by <b>July 20th 2020</b>.</p></li>
+				<li><p>After completing PART 1, send the <u>entire</u> form as a Word document to your <em>icipe</em> Primary Supervisor and your University Primary Supervisor by <b>July 30th 2020</b>.</p></li>
 				<li><p>Discuss the report with your supervisors.</p></li>
 				<li><p>.Ask your supervisors to complete, sign and date their respective part (PART 2: <em>icipe</em> Supervisor; PART 3: University Supervisor), and return to you by <u>August 7th 2020</u>. </p></li>
 				<li><p>Compile all parts of the report (PARTs 1, 2 and 3) into a single Word document.</p></li>
